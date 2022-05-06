f22207b56fdeba7277c81e11e42f232c29061506
unknown <you@example.com>
2022-05-06 19:27:11 +0700

    bai tap mang

---
+++ src/ss03_mang_va_phuong_thuc_trong_mang/bai_tap/TimGiaTriNhoNhatTrongMang.java
@@ -0,0 +1,35 @@
+package ss03_mang_va_phuong_thuc_trong_mang.bai_tap;
+
+import java.util.Scanner;
+
+public class TimGiaTriNhoNhatTrongMang {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập độ dài mảng: " + "\t");
+        size = sc.nextInt();
+
+        array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Nhập giá trị tại vị trí: " + i + "\t");
+            array[i] = sc.nextInt();
+        }
+        System.out.print("Hiển thị mảng: " + "\t");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
+        }
+
+        System.out.println("\n");
+        int min = array[0];
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                index = i;
+                min = array[i];
+            }
+        }
+
+        System.out.println("Giá trị nhỏ nhất trong mảng là: " + min + " tại vị trí " + index);
+    }
+}
---
+++ src/ss03_mang_va_phuong_thuc_trong_mang/bai_tap/TimPhanTuLonNhatMangHaiChieu.java
@@ -0,0 +1,46 @@
+package ss03_mang_va_phuong_thuc_trong_mang.bai_tap;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TimPhanTuLonNhatMangHaiChieu {
+    public static void main(String[] args) {
+        int soDong;
+        int soCot;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số dòng: ");
+        soDong = sc.nextInt();
+        System.out.print("Nhập số cột: ");
+        soCot = sc.nextInt();
+
+        int[][] array = new int[soDong][soCot];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print("nhập giá trị tại số dòng: " + i + " số cột " + j + "\t");
+                array[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Hiển thị mảng: ");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println("\n");
+        }
+
+
+
+        int max = array[0][0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (max < array[i][j]){
+                    max = array[i][j];
+                }
+            }
+        }
+
+        System.out.println("Phần tử lớn nhất là: " + max);
+    }
+}
